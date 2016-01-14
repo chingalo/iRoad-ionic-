2bed8bf174142880a24d266f08c7f66149604c27
Joseph Chingalo <profschingalo@gmail.com>
2016-01-14 17:02:50 +0300

    display driver verification data

--- www/js/controllers/driverController.js
+++ www/js/controllers/driverController.js
@@ -3,11 +3,47 @@ angular.module('app')
 .controller('DriverController',function($scope,ionicToast,$localStorage,$http){
 
     $scope.data = {};
+    $scope.data.loading = false;
 
+    function progressMessage(message){
+      ionicToast.show(message, 'bottom', false, 1500);
+    }
     $scope.verifyDriver = function(){
 
-      var message = 'Driver Licence Number : ' + $scope.data.driverLicenceNumber;
-      ionicToast.show(message, 'bottom', false, 1500);
+      if($scope.data.driverLicenceNumber){
+
+        $scope.data.loading = true;
+
+        var driverModal =  new iroad2.data.Modal('Driver',[]);
+        driverModal.get({'value':$scope.data.driverLicenceNumber},function(result){
+          if(result.length > 0){
+
+            $scope.data.loading = false;
+            $scope.data.driver = result[0];
+
+            var imageUrl = $localStorage.baseUrl + '/api/documents/';
+            if($scope.data.driver['Driver Photo']){
+              $scope.data.driverPhotoUrl = imageUrl + $scope.data.driver['Driver Photo'] + '/data';
+            }
+            else{
+              $scope.data.driverPhotoUrl = imageUrl + $scope.defaultPhotoID + '/data';
+            }
+            $scope.data.loading = false;
+            $scope.$apply();
+            console.log(JSON.stringify($scope.data.driver));
+          }else{
+            $scope.data.loading = false;
+            $scope.$apply();
+            var message = 'Driver has not Found';
+            progressMessage(message);
+          }
+        });
+
+      }else{
+
+        var message = 'Please Enter Driver Licence Number.';
+        progressMessage(message);
+      }
     };
 
     $scope.scanDriverLicence = function(){
@@ -15,17 +51,24 @@ angular.module('app')
       cordova.plugins.barcodeScanner.scan(
         function (result) {
           $scope.data.driverLicenceNumber = result.text;
+          var message = "Driver Licence Number has successfully scanned";
+          progressMessage(message);
           $scope.$apply();
           $scope.verifyDriver();
 
         },
         function () {
-          ionicToast.show('fail to scan', 'bottom', false, 1500);
+          var message = 'Fail to scan Driver Licence Number.';
+          progressMessage(message);
         }
       );
       $scope.verifyDriver();
     };
 
+
+    /*
+    getting default photo data
+     */
     $scope.defaultPhotoID = null;
     getDriverPhoto();
     function getDriverPhoto(){
